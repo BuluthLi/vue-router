0383b4ddfcf53b7578c5f039d353046b80abd801
BuluthLi <3195563138@qq.com>
2019-03-16 18:13:24 +0800

    【综合】综合react和vue的路由，加深理解

--- src/App.vue
+++ src/App.vue
@@ -28,19 +28,85 @@ export default {
     Header
   }
 };
+// react路由相关
+// 官网例子https://react-router.docschina.org/web/example/route-config
+// 总结，将所有路由统一到一张表中，实际的效果link和route视图还是一一对应(link和route视图一般放在一起)，就近原则（针对多个同层级的link只针对一个route视图。例如首尾都有导航菜单，点击任一处，都是控制中间的一个route视图层）
+// import React from "react";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// const Main = () => <h2>Main</h2>;
+
+// const Sandwiches = () => <h2>Sandwiches</h2>;
+
+// const Tacos = ({ routes }) => (
+//   <div>
+//     <h2>Tacos</h2>
+//     <ul>
+//       <li>
+//         <Link to="/tacos/bus">Bus</Link>
+//       </li>
+//       <li>
+//         <Link to="/tacos/cart">Cart</Link>
+//       </li>
+//     </ul>
+
+//     {routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
+//   </div>
+// );
+
+// const Bus = () => <h3>Bus</h3>;
+// const Cart = () => <h3>Cart</h3>;
+
+// // 路由表
+// const routes = [
+//   {
+//     path: "/sandwiches",
+//     component: Sandwiches
+//   },
+//   {
+//     path: "/tacos",
+//     component: Tacos,
+//     routes: [
+//       {
+//         path: "/tacos/bus",
+//         component: Bus
+//       },
+//       {
+//         path: "/tacos/cart",
+//         component: Cart
+//       }
+//     ]
+//   }
+// ];
+
+// const RouteWithSubRoutes = route => (
+//   <Route
+//     path={route.path}
+//     render={props => (
+//       <route.component {...props} routes={route.routes} />
+//     )}
+//   />
+// );
+
+// const RouteConfigExample = () => (
+//   <Router>
+//     <div>
+//       <ul>
+//         <li>
+//           <Link to="/tacos">Tacos</Link>
+//         </li>
+//         <li>
+//           <Link to="/sandwiches">Sandwiches</Link>
+//         </li>
+//       </ul>
+
+//       {routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)}
+//     </div>
+//   </Router>
+// );
+
+// export default RouteConfigExample;
 </script>
 
 <style scoped>
-/* #app {
-  display: flex;
-}
-.side-nav {
-}
-.nav-item {
-  display: block;
-}
-.hai {
-  background: red;
-  flex: 1;
-} */
 </style>
+
